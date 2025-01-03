52b36b5aea0439662d90e74d910b3d64d65b20a5
BiaPegado <132607617+BiaPegado@users.noreply.github.com>
2025-01-03 18:19:58 -0300

    Update train.py

--- train.py
+++ train.py
@@ -19,33 +19,26 @@ from sklearn.preprocessing import OrdinalEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
-# Separar classes
 class_majority = df[df['diabetes'] == 0]
 class_minority = df[df['diabetes'] == 1]
 
-# Subamostrar a classe majoritária
 class_majority_downsampled = resample(class_majority,
                                       replace=False,
                                       n_samples=len(class_minority),
                                       random_state=42)
 
-# Combinar classes
 df = pd.concat([class_majority_downsampled, class_minority])
 
 from pycaret.classification import *
 
-# Criação do objeto de experimento
 exp = ClassificationExperiment()
 
-# Inicialização do setup no objeto
 exp.setup(df, target='diabetes', session_id=123)
 
-# Comparar modelos
 best = exp.compare_models()
 best_tuned = exp.tune_model(best)
 print(best_tuned)
 
-# plot feature importance
 exp.plot_model(best, plot = 'feature')
 
 import shutil
@@ -63,15 +56,12 @@ os.makedirs(dest, exist_ok=True)
 filename = "Confusion Matrix.png"
 exp.plot_model(final_model, plot="confusion_matrix", save=True)
 shutil.move(filename, os.path.join(dest, filename))
-# Fazer previsões com o modelo final
-predictions = exp.predict_model(final_model)
-# Obter as variáveis 'target' e 'predicted' das previsões
-y_true = predictions['diabetes']  # Supondo que a coluna verdadeira seja 'Label'
-y_pred = predictions['prediction_label']  # Supondo que a coluna de predições seja 'Prediction'
 
-# Calcular a acurácia
+predictions = exp.predict_model(final_model)
+y_true = predictions['diabetes']  
+y_pred = predictions['prediction_label']  
+
 accuracy = accuracy_score(y_true, y_pred)
 
-# Salvar a acurácia no arquivo
 with open('Results/metrics.txt', 'w') as f:
     f.write(f"Acuracia do modelo: {round(accuracy, 3)}\n")
