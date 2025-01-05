81714a4a1f6c36051d1de7b7ad9f1bce14a29f62
boop014 <abiapegado014@gmail.com>
2025-01-05 15:59:54 +0000

    Update with new results

--- train.py
+++ train.py
@@ -1,11 +1,11 @@
 import pandas as pd
 from pycaret import *
 
-df = pd.read_csv('Data/diabetes_prediction_dataset.csv')
+df = pd.read_csv("Data/diabetes_prediction_dataset.csv")
 
 df.info()
 
-class_counts = df['diabetes'].value_counts()
+class_counts = df["diabetes"].value_counts()
 
 print("\nContagem das Classes na Coluna 'diabetes':")
 print(f"Classe 0 (Não diabético): {class_counts[0]}")
@@ -19,13 +19,12 @@ from sklearn.preprocessing import OrdinalEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
-class_majority = df[df['diabetes'] == 0]
-class_minority = df[df['diabetes'] == 1]
+class_majority = df[df["diabetes"] == 0]
+class_minority = df[df["diabetes"] == 1]
 
-class_majority_downsampled = resample(class_majority,
-                                      replace=False,
-                                      n_samples=len(class_minority),
-                                      random_state=42)
+class_majority_downsampled = resample(
+    class_majority, replace=False, n_samples=len(class_minority), random_state=42
+)
 
 df = pd.concat([class_majority_downsampled, class_minority])
 
@@ -33,28 +32,28 @@ from pycaret.classification import *
 
 exp = ClassificationExperiment()
 
-exp.setup(df, target='diabetes', session_id=123)
+exp.setup(df, target="diabetes", session_id=123)
 
 best = exp.compare_models()
 best_tuned = exp.tune_model(best)
 print(best_tuned)
 
-exp.plot_model(best, plot = 'feature')
+exp.plot_model(best, plot="feature")
 
 import shutil
 import os
 from sklearn.metrics import accuracy_score
 
 final_model = exp.finalize_model(best_tuned)
-exp.save_model(final_model, 'Model/diabetes_model')
+exp.save_model(final_model, "Model/diabetes_model")
 
 evaluation_results = exp.evaluate_model(final_model)
 
 predictions = exp.predict_model(final_model)
-y_true = predictions['diabetes']  
-y_pred = predictions['prediction_label']  
+y_true = predictions["diabetes"]
+y_pred = predictions["prediction_label"]
 
 accuracy = accuracy_score(y_true, y_pred)
 
-with open('Results/metrics.txt', 'w') as f:
+with open("Results/metrics.txt", "w") as f:
     f.write(f"Acuracia do modelo: {round(accuracy, 3)}\n")
