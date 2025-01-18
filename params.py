3251b8cd77ddeb490130bcaa9a90a903deed76ac
boop014 <abiapegado014@gmail.com>
2025-01-18 22:44:22 +0000

    Update with new results

--- params.py
+++ params.py
@@ -1,11 +1,25 @@
 WANDB_PROJECT = "diabetes-classification"
-ENTITY = None 
-BDD_CLASSES = {i:c for i,c in enumerate(['gender', 'age', 'hypertension', 'heart_disease', 'smoking_history',
-       'bmi', 'HbA1c_level', 'blood_glucose_level', 'diabetes'])}
-RAW_DATA_AT = 'diabetes'
-PROCESSED_DATA_AT = 'diabetes_processed'
+ENTITY = None
+BDD_CLASSES = {
+    i: c
+    for i, c in enumerate(
+        [
+            "gender",
+            "age",
+            "hypertension",
+            "heart_disease",
+            "smoking_history",
+            "bmi",
+            "HbA1c_level",
+            "blood_glucose_level",
+            "diabetes",
+        ]
+    )
+}
+RAW_DATA_AT = "diabetes"
+PROCESSED_DATA_AT = "diabetes_processed"
 CONFIG = {
-        "model": "diabetes_model",
-        "dataset": "Diabetes dataset",
-        "metric": "Acurácia"
-    }
+    "model": "diabetes_model",
+    "dataset": "Diabetes dataset",
+    "metric": "Acurácia",
+}
